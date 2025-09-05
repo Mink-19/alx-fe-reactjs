37951ee62f4150d471c83ac5cb661980e52edbac
Phelly <pheliciaawino999@gmail.com>
2025-09-05 23:39:19 +0300

    Updated changes

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,8 +1,22 @@
 // src/components/Footer.jsx
 const Footer = () => {
   return (
-    <footer style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <p style={{ color: 'blue' }}>&copy; 2025 My React App. All rights reserved.</p>
+     <footer
+      style={{
+        borderTop: "2px solid gray",
+        padding: "15px",
+        margin: "10px",
+        backgroundColor: "midnightblue",
+        color: "white",
+        textAlign: "center",
+        borderRadius: "8px"
+      }}
+      
+    >
+      
+      <p style={{ fontSize: "14px" }}>
+        &copy; 2025 My React App. All rights reserved.
+      </p>
     </footer>
   );
 };
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,20 @@
 // src/components/MainContent.jsx
 const MainContent = () => {
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue' }}>Main Content</h2>
+   
+    
+    <div
+      style={{
+        border: "1px solid gray",
+        padding: "15px",
+        margin: "10px",
+        backgroundColor: "pink",
+        borderRadius: "8px",
+        textAlign: "center"
+      }}
+    >
+      <h2 style={{ color: "red", marginBottom: "8px" }}>Main Content</h2>
+      <p style={{ fontSize: "16px", lineHeight: "1.5" }}></p>
       <p>I love to visit New York, Paris, and Tokyo.</p>
     </div>
   );
