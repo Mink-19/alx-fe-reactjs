7e506ba3e04549e2cfdd06c16dd802fc46429178
Phelly <pheliciaawino999@gmail.com>
2025-09-07 13:52:10 +0300

    Updated files for prop drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,18 +1,17 @@
-// src/App.jsx
 import ProfilePage from './ProfilePage';
-import UserContext from './UserContext'; 
+import UserContext from './UserContext';
 
 function App() {
-  // User data to be shared via context
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    // Provide userData to all child components using UserContext
     <UserContext.Provider value={userData}>
       <ProfilePage />
     </UserContext.Provider>
   );
 }
+
+
 
 export default App;
 
--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,19 +1,13 @@
 import { useContext } from 'react';
 import UserContext from './UserContext';
-
 function UserDetails() {
-  const userData = useContext(UserContext); 
-
-  
-  if (!userData) return <p>Loading user data...</p>;
-
+  const userData = useContext(UserContext);
   return (
     <div>
-      <p>Name: {userData.name}</p>
+      <p>Name: {userData.name} </p>
       <p>Email: {userData.email}</p>
     </div>
   );
 }
 
 export default UserDetails;
-
