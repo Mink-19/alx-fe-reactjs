55d83c828c5df03abc2f1a0f2176552c0c7668c6
Phelly <pheliciaawino999@gmail.com>
2025-09-30 18:05:31 +0300

    Question 0

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // 'media' or 'class' can be used if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
